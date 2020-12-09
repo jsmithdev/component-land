7a2295d5b2212f9f2d4cf32c2ae7f32c1096b672
Jamie Smith <me@jsmith.dev>
2020-12-09 09:16:32 -0500

    header work for search, filter, mobile/desk

--- src/modules/land/header/header.js
+++ src/modules/land/header/header.js
@@ -1,6 +1,7 @@
+/* eslint-disable @lwc/lwc/no-api-reassignments */
 import { api, LightningElement } from 'lwc';
 
-export default class App extends LightningElement {
+export default class LandHeader extends LightningElement {
     @api icon;
 
     @api
@@ -10,14 +11,58 @@ export default class App extends LightningElement {
     }
     set items(object) {
         this._items = Object.keys(object).map(key => object[key]);
-        console.dir(JSON.parse(JSON.stringify(this._items)));
+        if(!this._init_items){
+            this._init_items = Array.from( this._items );
+        }
+        console.dir(JSON.parse(JSON.stringify({
+            items: this._items,
+            object,
+        })));
     }
+    
+    setValue(event){
+        this.value = event.target.value;
+        console.log(this.value)
+        this.dispatchEvent( new CustomEvent('search', {
+            bubbles: true,
+            composed: true,
+            detail: { value: this.value }
+        }))
+    }
+    hotkeys(event){
 
-    connectedCallback() {
-        //console.dir(this.title)
+        this.value = event.target.value;
+        
+        if(event.code === 'Enter'){ this.search(); return; }
+        if(event.code === 'Esc'){ this.value = ''; this.searchValue = ''; return; }
+
+        this.dispatchEvent( new CustomEvent('search', {
+            bubbles: true,
+            composed: true,
+            detail: { value: this.value }
+        }))
     }
-    /* 
-    const result = await fetch('/contact')
-    console.log(result)
-    */
+    search(){
+        this.dispatchEvent( new CustomEvent('search', {
+            bubbles: true,
+            composed: true,
+            detail: { value: this.value }
+        }))
+    }
+    filter(event){
+
+        const {
+            platform,
+            type,
+        } = event.target.options[event.target.selectedIndex].dataset
+
+        this.dispatchEvent( new CustomEvent('filter', {
+            bubbles: true,
+            composed: true,
+            detail: { 
+                platform,
+                type
+            }
+        }))
+    }
 }
